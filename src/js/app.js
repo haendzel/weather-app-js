ab1fda06f9c147e3e499de5267cb6e538e4c97ce
Filip Handzel <f.handzel@gmail.com>
2020-02-04 09:33:04 +0100

    change promise type

--- src/js/app.js
+++ src/js/app.js
@@ -8,10 +8,7 @@ const kelvin = 273;
 const key = "b5a3cbfba158fdfca9ef2456457088c0"; //api key
 
 // const weather = {
-//     temperature: {
-//         value: 21,
-//     },
-
+//     temperature: 21,
 //     description: 'sunny',
 //     icon : "sunny",
 //     city: "Kraków",
@@ -43,19 +40,19 @@ function showError(error) {
 //get weather from api data
 function getWeather(latitude, longitude) {
     let api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + key;
-    fetch(api)
-    .then(function(response){
+    fetch(api).then(response => {
+        //console.log(response.status);
         let data = response.json();
         return data;
     })
-    .then(function(data){
-        weather.temperature.value = Math.floor(data.main.temp - kelvin);
+    .then(data => {
+        weather.temperature = Math.floor(data.main.temp - kelvin);
         weather.description = data.weather[0].description;
         weather.icon = data.weather[0].icon;
         weather.city = data.name;
         weather.country = data.sys.country;
     })
-    .then(function (){
+    .then(() => {
         displayWeather();
     });
 }
@@ -63,7 +60,7 @@ function getWeather(latitude, longitude) {
 //display in web
 function displayWeather() {
     iconWeather.innerHTML = `<img src="icons/${weather.icon}.svg"/>`;
-    temp.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    temp.innerHTML = `${weather.temperature}°<span>C</span>`;
     desc.innerHTML = weather.description;
     zone.innerHTML = `${weather.city}, ${weather.country}`;
 };
